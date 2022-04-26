668c187ed08ecc855b940cbd24192c7fc5f210c4
LAPTOP-7K6SKONF\zayn <pengb@tcd.ie>
2022-04-26 14:52:52 +0100

    Add Rating UI and modify the table display

--- react-frontend/src/components/find.jsx
+++ react-frontend/src/components/find.jsx
@@ -3,6 +3,7 @@ import { Rate, Table, Modal } from "antd";
 import { Row, Col } from "antd";
 import "antd/dist/antd.min.css";
 import "../bootstrap.min.css";
+import { AiOutlineCloseCircle,AiOutlineCheckCircle} from "react-icons/ai";
 
 export default class Find extends React.Component {
   showDetail(name) {
@@ -55,88 +56,202 @@ export default class Find extends React.Component {
           name:"Restaurant 1",
           location:"somewhere",
           star:5,
-          info:"here is the info of rest #1"
+          BusinessAcceptsCreditCards:true,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:true,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:true, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:2,
           name:"Restaurant 2",
           location:"somewhere",
           star:4,
-          info:"here is the info of rest #2"
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"u'full_bar'",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:2,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:3,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:4,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:5,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:6,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:7,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:8,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:9,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
         },
         {
           key:10,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"},
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
+        },
         {
           key:11,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"},
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
+        },
         
         {
           key:12,
           name:"Restaurant 3",
           location:"somewhere",
           star:3,
-          info:"here is the info of rest #3"},
+          BusinessAcceptsCreditCards:false,
+          Alcohol:"none",
+          Bikeparking:false,
+          CoatCheck:false,
+          Wifi:true,
+          GoodForKids:false, 
+          OutdoorSeating:false, 
+          RestaurantsPriceRange2:1,
+          RestaurantsReservations:false,
+          Smoking:false,
+        },
         ],
         
       selectedRest : null,
       detailModalVisible  : true
     };
+    // this.setState={
+    //   // todo
+    // }
     this.showDetail = this.showDetail.bind(this);
   }
 
@@ -158,7 +273,98 @@ export default class Find extends React.Component {
               this.setState({ detailModalVisible: false });
             }}
           >
-            <p>{this.state.selectedRest.info}</p>
+            <Row>
+              <Col>
+                Business CreditCards Accepted: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.BusinessAcceptsCreditCards?<AiOutlineCheckCircle />:<AiOutlineCloseCircle />}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Alcohol Avaliable: &nbsp;
+              </Col>
+              <Col>
+              {this.state.selectedRest.Alcohol==="none"&&<AiOutlineCloseCircle />}
+               {this.state.selectedRest.Alcohol==="u'full_bar'"&&"Full bar"}
+               {this.state.selectedRest.Alcohol==="'beer_and_wine''"&&"Bear and Wine"}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Bike Parking Avaliable: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.Bikeparking?<AiOutlineCheckCircle />:<AiOutlineCloseCircle />}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Coat Check Avaliable: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.CoatCheck?<AiOutlineCheckCircle />:<AiOutlineCloseCircle />}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Wifi Avaliable: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.Wifi?<AiOutlineCheckCircle />:<AiOutlineCloseCircle />}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+               Good For Kids: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.GoodForKids?<AiOutlineCheckCircle />:<AiOutlineCloseCircle />}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Outdoor Seating Avaliable: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.OutdoorSeating?<AiOutlineCheckCircle />:<AiOutlineCloseCircle />}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Restaurants Price Range: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.RestaurantsPriceRange2===1&&"$"} {this.state.selectedRest.RestaurantsPriceRange2===2&&"$$"} {this.state.selectedRest.RestaurantsPriceRange2===3&&"$$$"}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Restaurants Reservations Avaliable: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.RestaurantsReservations?<AiOutlineCheckCircle />:<AiOutlineCloseCircle />}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Smoking Allowed: &nbsp;
+              </Col>
+              <Col>
+                {this.state.selectedRest.Smoking?<AiOutlineCheckCircle />:<AiOutlineCloseCircle />}
+              </Col>
+            </Row>
+            {/* <p>BusinessAcceptsCreditCards<p>{this.state.selectedRest.BusinessAcceptsCreditCards}</p></p>
+            <p>Alcohol<p>{this.state.selectedRest.Alcohol}</p></p>
+            <p>Bikeparking<p>{this.state.selectedRest.Bikeparking}</p></p>
+            <p>CoatCheck<p>{this.state.selectedRest.CoatCheck}</p></p>
+            <p>Wifi<p>{this.state.selectedRest.Wifi}</p></p>
+            <p>GoodForKids<p>{this.state.selectedRest.GoodForKids}</p></p>
+            <p>OutdoorSeating<p>{this.state.selectedRest.OutdoorSeating}</p></p>
+            <p>RestaurantsPriceRange2<p>{this.state.selectedRest.RestaurantsPriceRange2}</p></p>
+            <p>RestaurantsReservations<p>{this.state.selectedRest.RestaurantsReservations}</p></p>
+            <p>Smoking<p>{this.state.selectedRest.Smoking}</p></p> */}
             <Rate allowHalf defaultValue={2.5} />
             <button  className=" d-block btn-primary btn-user w-20 h-10" type="submit">Submit</button>
           </Modal>
